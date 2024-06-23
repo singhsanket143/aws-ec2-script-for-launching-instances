6c1145fb6f379eaf13a8413c3a242610e2df57c1
Sanket Singh <singhsanket143@gmail.com>
2024-06-23 19:19:13 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Update the ubuntu machine
-sudo apt update
+sudo apt update -y
 
 # Install nodejs in the machine
-sudo apt install nodejs
+sudo apt install nodejs -y
 
 # Install npm in the machine
-sudo apt install npm
+sudo apt install npm -y
 
 # Clone the repository
 git clone https://github.com/singhsanket143/AWS-Demo-Project.git /home/ubuntu/project
