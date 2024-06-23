26109cbd665b7fa8bebd3baf26c16ff23bcc8b84
Sanket Singh <singhsanket143@gmail.com>
2024-06-23 18:48:29 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update the ubuntu machine
+sudo apt update
+
+# Install nodejs in the machine
+sudo apt install nodejs
+
+# Install npm in the machine
+sudo apt install npm
+
+# Clone the repository
+git clone https://github.com/singhsanket143/AWS-Demo-Project.git
+
+# cd in the folder
+cd AWS-Demo-Project
+
+# install packages
+npm install
+
+# turn up the server
+npx pm2 start index.js
